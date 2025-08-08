71d9b587d3a87599c00ee396138def2f1099ce1c
wewon-backdown <andrevano2000@gmail.com>
2025-08-09 03:41:29 +0400

    gh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: '/rin-landing/',
   plugins: [react(), tailwindcss()],
   publicDir: './public',
   server: {
